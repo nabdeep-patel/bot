16a7f276e8eecedc383c7b74969ab05ff85f4a78
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-30 18:23:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,16 +76,10 @@ hide_streamlit_style = """
 <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
+#GithubIcon {
+  visibility: hidden;}
 </style>
 
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 import streamlit as st
-
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
